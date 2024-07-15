91233e4125cfa9bd41f50d640e6ce1551c2e4a88
hugocruzlfc <hugocruzlfc@gmail.com>
2024-07-15 10:59:03 +0200

    feat: add domain files

---
+++ src/domain/me/me.ts
@@ -0,0 +1,15 @@
+import { User } from "../user";
+
+export const MAX_NUM_SHOUTS_PER_DAY = 5;
+
+export interface Me extends User {
+  numShoutsPastDay: number;
+}
+
+export function isAuthenticated(me?: Me | null): me is Me {
+  return Boolean(me);
+}
+
+export function hasExceededShoutLimit(me: Me) {
+  return me.numShoutsPastDay >= MAX_NUM_SHOUTS_PER_DAY;
+}
